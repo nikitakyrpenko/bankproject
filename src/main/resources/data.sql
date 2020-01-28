690282fb9367e0c93335ccb4ffe8449364db7005
nikitakyrpenko@gmail.com <nikitakyrpenko@gmail.com>
2020-01-29 00:52:30 +0200

    update schema and data for db

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,80 +1,74 @@
 
-INSERT INTO `roles` (`role_role`) VALUES ("USER");
-INSERT INTO `roles` (`role_role`) VALUES ("ADMIN");
+INSERT INTO `roles` (`role_description`)
+VALUES
+("USER"),
+("ADMIN");
 
-
-INSERT INTO `users` (`user_name`,`user_surname`,`user_email`,`user_telephone`,`fk_role`) 
-VALUES 
-("Freya","Rodriguez","dolor.Donec@etmagnaPraesent.net","715-0987","1"),
-("Fleur","Morgan","Vivamus.rhoncus.Donec@lacusEtiam.net","612-4806","2"),
-("Kim","Butler","ante.iaculis@Donecnibhenim.ca","451-2416","1"),
-("Amaya","Davis","Vivamus.euismod.urna@ligulaAeneangravida.edu","338-6234","2"),
-("Kellie","Jackson","rutrum.eu.ultrices@ultricesposuerecubilia.net","1-310-474-8458","1"),
-("Clinton","Mckay","erat.volutpat@nibhDonec.net","168-6165","2"),
-("Wesley","Merritt","lobortis@sitametrisus.ca","136-2862","1"),
-("Cameron","Beasley","tellus.Suspendisse@magnamalesuadavel.com","770-3791","2"),
-("Blossom","Cole","quis@auctorvelit.org","219-8751","1"),
-("Mollie","Macias","mauris.Morbi.non@arcu.org","1-856-583-3206","2");
-
-INSERT INTO `deposit_accounts` (`deposit_account_rate`) VALUES 
-(3),
-(14),
-(15),
-(6),
-(14),
-(16),
-(20),
-(12),
-(18),
-(14);
-
-
-INSERT INTO `credit_accounts` (`credit_account_limit`,`credit_rate`) VALUES 
-(7156,12),
-(2955,2),
-(4422,11);
-
-
-INSERT INTO `accounts` (`expiration_date`,`balance`,`fk_user_account`,`fk_account_deposit_account`,`fk_account_credit_account`) 
-VALUES 
-("2020-12-28 20:43:41",1788,1,1,null),
-("2020-06-01 09:17:23",64959,2,2,null),
-("2019-10-25 05:24:21",89242,3,null,1),
-("2019-03-13 20:32:11",3797,4,null,2),
-("2020-06-01 09:50:35",80925,5,5,null),
-("2019-04-03 20:35:57",66227,6,6,null),
-("2019-09-04 21:42:47",15487,7,7,null),
-("2019-10-30 03:14:42",66663,8,8,null),
-("2020-05-18 10:59:51",75516,9,9,null),
-("2020-10-28 22:12:15",77593,10,null,3);
-
-INSERT INTO `operations_type` (`operation_type_description`) VALUES 
-("WITHDRAWAL"),
-("ARRIVAL"),
+INSERT INTO `charge_types`(`charge_description`)
+VALUE
 ("DEPOSIT_ARRIVAL"),
 ("CREDIT_ARRIVAL");
 
-INSERT INTO `operations` (`operation_purpose`,`operation_date`,`operation_transfer`,`fk_operation_type`) VALUES 
- ("Aliquam erat volutpat.","2020-08-16 13:57:11",65305,4),
- ("ipsum leo","2020-11-22 16:19:32",19866,2),
- ("at, nisi.","2019-06-19 01:05:53",70987,2),
- ("eget metus. In","2019-08-12 22:10:12",57559,2),
- ("Vivamus molestie","2019-06-10 08:24:11",84986,3),
- ("sed","2020-10-23 04:56:20",98213,4),
- ("diam luctus lobortis.","2021-01-11 23:54:46",35457,1),
- ("nec, euismod in,","2019-05-31 01:49:21",64770,1),
- ("pretium","2020-03-12 12:12:36",3362,3),
- ("urna. Ut tincidunt","2020-11-07 23:19:28",45139,3),
- ("eu, ligula.","2020-03-17 13:44:01",42686,3),
- ("hendrerit","2021-01-03 16:07:56",32746,4),
- ("varius. Nam porttitor","2021-01-23 15:20:12",52567,2),
- ("ligula. Aenean","2019-07-28 09:51:20",49014,3),
- ("quis, pede.","2020-12-01 09:40:37",3977,4),
- ("a sollicitudin orci","2020-01-16 04:19:35",35720,2),
- ("iaculis","2020-03-03 12:06:32",66096,3),
- ("eu","2020-11-23 03:24:42",43977,1),
- ("tortor.","2019-02-12 07:28:52",80161,2),
- ("pellentesque,","2019-05-26 22:05:15",28397,3);
+
+INSERT INTO `accounts_type`(`type_description`)
+VALUE
+("DEPOSIT"),
+("CREDIT");
+
+INSERT INTO `users` (`firstname`,`surname`,`email`,`passwords`,`telephone`,`fk_roles_users`)
+VALUES 
+("Freya","Rodriguez","dolor.Donec@etmagnaPraesent.net","1","715-0987","1"),
+("Fleur","Morgan","Vivamus.rhoncus.Donec@lacusEtiam.net","1","612-4806","1"),
+("Kim","Butler","ante.iaculis@Donecnibhenim.ca","451-2416","1","1"),
+("Amaya","Davis","Vivamus.euismod.urna@ligulaAeneangravida.edu","1","338-6234","1"),
+("Kellie","Jackson","rutrum.eu.ultrices@ultricesposuerecubilia.net","1","1-310-474-8458","1"),
+("Clinton","Mckay","erat.volutpat@nibhDonec.net","168-6165","1","1"),
+("Wesley","Merritt","lobortis@sitametrisus.ca","136-2862","1","1"),
+("Cameron","Beasley","tellus.Suspendisse@magnamalesuadavel.com","1","770-3791","1"),
+("Blossom","Cole","quis@auctorvelit.org","219-8751","1","1"),
+("Mollie","Macias","mauris.Morbi.non@arcu.org","1-856-583-3206","1","1");
+
+
+INSERT INTO `operations` (`purpose`,`operation_date`,`transfer`) VALUES
+ ("Aliquam erat volutpat.","2020-08-16 13:57:11",65305),
+ ("ipsum leo","2020-11-22 16:19:32",19866),
+ ("at, nisi.","2019-06-19 01:05:53",70987),
+ ("eget metus. In","2019-08-12 22:10:12",57559),
+ ("Vivamus molestie","2019-06-10 08:24:11",84986),
+ ("sed","2020-10-23 04:56:20",98213),
+ ("diam luctus lobortis.","2021-01-11 23:54:46",35457),
+ ("nec, euismod in,","2019-05-31 01:49:21",64770),
+ ("pretium","2020-03-12 12:12:36",3362),
+ ("urna. Ut tincidunt","2020-11-07 23:19:28",45139),
+ ("eu, ligula.","2020-03-17 13:44:01",42686),
+ ("hendrerit","2021-01-03 16:07:56",32746),
+ ("varius. Nam porttitor","2021-01-23 15:20:12",52567),
+ ("ligula. Aenean","2019-07-28 09:51:20",49014),
+ ("quis, pede.","2020-12-01 09:40:37",3977),
+ ("a sollicitudin orci","2020-01-16 04:19:35",35720),
+ ("iaculis","2020-03-03 12:06:32",66096),
+ ("eu","2020-11-23 03:24:42",43977),
+ ("tortor.","2019-02-12 07:28:52",80161),
+ ("pellentesque,","2019-05-26 22:05:15",28397);
+
+
+INSERT INTO `accounts` (`expiration_date`,`balance`,`deposit_account_rate`,`fk_users_accounts`,`fk_accounts_type_accounts`)
+VALUES
+("2020-09-06 13:48:20",88732,0.3,1,1),
+("2020-05-13 14:10:42",55339,0.12,2,1),
+("2019-07-19 16:00:38",21912,0.1,3,1),
+("2019-08-19 21:35:21",18086,0.1,4,1),
+("2019-03-27 15:27:37",22737,0.7,5,1),
+("2019-04-01 05:36:09",12630,0.3,6,1),
+("2020-03-13 10:27:05",10371,0.15,7,1);
+
+
+INSERT INTO `accounts` (`expiration_date`,`balance`,`credit_limit`,`credit_rate`,`fk_users_accounts`,`fk_accounts_type_accounts`)
+VALUES
+("2020-09-06 13:48:20",88732,100000,0.12,8,2),
+("2020-05-13 14:10:42",55339,100000,0.12,9,2),
+("2019-07-19 16:00:38",21912,100000,0.12,10,2);
+
 
 
 INSERT INTO `bank_database`.`accounts_operations` (`fk_accounts_sender`, `fk_accounts_receiver`, `fk_operations_operations_id`) VALUES ('1', '2', '1');
@@ -97,3 +91,9 @@ INSERT INTO `bank_database`.`accounts_operations` (`fk_accounts_sender`, `fk_acc
 INSERT INTO `bank_database`.`accounts_operations` (`fk_accounts_sender`, `fk_accounts_receiver`, `fk_operations_operations_id`) VALUES ('4', '2', '18');
 INSERT INTO `bank_database`.`accounts_operations` (`fk_accounts_sender`, `fk_accounts_receiver`, `fk_operations_operations_id`) VALUES ('1', '10', '19');
 INSERT INTO `bank_database`.`accounts_operations` (`fk_accounts_sender`, `fk_accounts_receiver`, `fk_operations_operations_id`) VALUES ('9', '2', '20');
+
+
+INSERT INTO `charges`
+(`charge`,`fk_charge_types_charge`,`fk_account_charge`)
+VALUES
+(581,2,10),(967,1,1),(521,1,5),(660,1,1),(570,1,3),(933,1,4),(563,1,6),(303,2,8),(726,2,7),(444,2,9);
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,88 +1,105 @@
+DROP TABLE IF EXISTS `roles`;
 create table if not exists roles (
-  role_id int(10) not null auto_increment, 
-  role_role    varchar(255) not null, 
-  primary key (role_id),
-  unique index (role_id));
-  
+  id                  int(10) not null auto_increment,
+  role_description    varchar(255) not null,
+  primary key (id),
+  unique index (id));
+
+DROP TABLE IF EXISTS `users`;
 create table if not exists users (
-  user_id   int(10) not null unique  auto_increment, 
-  fk_role   int(10) not null, 
-  user_name      varchar(255) not null, 
-  user_surname   varchar(255) not null, 
-  user_email     varchar(255) not null unique, 
-  user_telephone varchar(255) not null, 
-  primary key (user_id),
-  unique index (user_id),
-  foreign key (fk_role) REFERENCES roles(role_id)
+  id               int(10)     not null unique  auto_increment,
+  firstname        varchar(15) not null,
+  surname          varchar(15) not null,
+  email            varchar(50) not null unique,
+  passwords        varchar(100)not null,
+  telephone        varchar(15) not null,
+  fk_roles_users   int(10)     not null,
+  primary key (id),
+  unique index (id),
+  foreign key (fk_roles_users) REFERENCES roles(id)
   ON UPDATE NO ACTION
   ON DELETE NO ACTION);
-  
-create table if not exists credit_accounts (
-  credit_account_id int(10) not null auto_increment, 
-  credit_account_limit      decimal(10, 0) not null, 
-  credit_rate               decimal(5, 0) not null, 
-  charge_per_month          decimal(10, 0) default 0, 
-  credit_liabilities        decimal(10, 0) default 1, 
-  primary key (credit_account_id), 
-  unique index (credit_account_id));
-  
-create table if not exists deposit_accounts (
-  deposit_account_id        int(10) not null auto_increment, 
-  deposit_account_rate      decimal(10, 0) not null, 
-  primary key (deposit_account_id), 
-  unique index (deposit_account_id));
-  
-create table if not exists accounts (
-  account_id                 int(10) not null auto_increment, 
-  expiration_date            date not null, 
-  balance                    decimal(10, 0) not null, 
-  fk_user_account            int(10) not null, 
-  fk_account_deposit_account int(10) DEFAULT -1, 
-  fk_account_credit_account  int(10) DEFAULT -1, 
-  primary key (account_id),
-  unique index (account_id),
-  foreign key (fk_user_account)  REFERENCES users(user_id)
-  on update CASCADE
-  on delete CASCADE  ,
-  foreign key (fk_account_credit_account) REFERENCES credit_accounts(credit_account_id)
-  on update CASCADE
-  on delete CASCADE,
-  foreign key (fk_account_deposit_account) REFERENCES deposit_accounts(deposit_account_id)
-  on update CASCADE
-  on delete CASCADE,
-  unique (fk_account_credit_account, fk_account_deposit_account));
-  
-create table if not exists operations_type(
-  operation_type_id int(10) not null auto_increment,
-  operation_type_description varchar(255) not null,
-  primary key(operation_type_id),
-  unique index (operation_type_id));
-  
+
+
+ DROP TABLE IF EXISTS `account_types`;
+ create table if not exists accounts_type(
+	id               int(10) not null auto_increment,
+    type_description varchar(20) not null,
+    primary key (id),
+    unique index (id)
+ );
+
+     DROP TABLE IF EXISTS `accounts`;
+ create table if not exists accounts (
+  id                        int(10) not null auto_increment,
+  expiration_date           date not null,
+  balance                   double not null,
+  deposit_account_rate      double default 0.0,
+  credit_limit      		double default 0.0,
+  credit_rate               double default 0.0,
+  charge_per_month          double default 0.0,
+  credit_liabilities        double default 0.0,
+  fk_users_accounts         int(10) not null,
+  fk_accounts_type_accounts int(10) not null,
+  primary key (id),
+  unique index (id),
+  foreign key (fk_users_accounts)  REFERENCES users(id)
+  on update NO ACTION
+  on delete NO ACTION,
+  foreign key (fk_accounts_type_accounts)  REFERENCES accounts_type(id)
+  on update NO ACTION
+  on delete NO ACTION);
+
+ DROP TABLE IF EXISTS `charge_types`;
+ CREATE TABLE IF NOT EXISTS charge_types(
+	id                 int(10) not null auto_increment,
+	charge_description varchar(20) not null,
+	primary key (id),
+    unique index (id)
+ );
+
+
+ DROP TABLE IF EXISTS `charges`;
+ CREATE TABLE IF NOT EXISTS charges(
+	id                     int(10) not null auto_increment,
+	charge                 double not null,
+	fk_charge_types_charge int(10) not null,
+	fk_account_charge      int(10) not null,
+	PRIMARY KEY (id),
+    UNIQUE  INDEX (id),
+	foreign key (fk_charge_types_charge)  REFERENCES charge_types(id)
+	on update NO ACTION
+    on delete NO ACTION,
+	foreign key (fk_account_charge)  REFERENCES accounts(id)
+	on update NO ACTION
+    on delete NO ACTION);
+
+
+DROP TABLE IF EXISTS `operations`;
 create table if not exists operations(
-   operation_id int(10) not null auto_increment,
-   operation_purpose varchar(255) not null,
-   operation_date DATE not null,
-   operation_transfer decimal(10,0) not null,
-   fk_operation_type int(10) not null,
-   primary key(operation_id),
-   unique index (operation_id),
-   foreign key (fk_operation_type) REFERENCES operations_type(operation_type_id)
-   on update NO ACTION
-   on delete NO ACTION);
-   
+   id                            int(10) not null auto_increment,
+   purpose                       varchar(255) not null,
+   operation_date                DATE not null,
+   transfer                      double not null,
+   primary key(id),
+   unique index (id));
+
+
+DROP TABLE IF EXISTS `accounts_operations`;
 create table if not exists accounts_operations(
-    accounts_operations_id int(10) not null auto_increment,
-    fk_accounts_sender int(10) not null,
-    fk_accounts_receiver int(10) not null,
+    id                          int(10) not null auto_increment,
+    fk_accounts_sender          int(10) not null,
+    fk_accounts_receiver        int(10) not null,
     fk_operations_operations_id int(10) not null,
-    primary key(accounts_operations_id),
-    unique index (accounts_operations_id),
-    foreign key(fk_operations_operations_id) REFERENCES operations(operation_id)
+    primary key(id),
+    unique index (id),
+    foreign key(fk_operations_operations_id) REFERENCES operations(id)
     ON UPDATE NO ACTION
     ON DELETE NO ACTION,
-    foreign key (fk_accounts_sender) REFERENCES accounts(account_id)
+    foreign key (fk_accounts_sender) REFERENCES accounts(id)
     ON UPDATE NO ACTION
     ON DELETE NO ACTION,
-    foreign key (fk_accounts_receiver) REFERENCES accounts(account_id)
+    foreign key (fk_accounts_receiver) REFERENCES accounts(id)
     ON UPDATE NO ACTION
     ON DELETE NO ACTION);
+
